9feefebe70e0b3c6364d19ac54070ea717b826ca
Wojtek Wiechec <wojciech.wiechec@solutions4delivery.com>
2024-02-18 11:57:48 +0100

    Changes to Invoice class to match unitTests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,48 +3,71 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-//    private Collection<Product> products = new ArrayList<>();
-    private Map<Product, Integer> products = new HashMap<>();
+    private Collection<Product> products;
 
-    private BigDecimal subTotal;
+    private BigDecimal netPrice;
 
-    private  BigDecimal tax;
+    private BigDecimal tax;
 
-    private  BigDecimal total;
+    private BigDecimal grossPrice;
 
     public Invoice() {
-
+        this.netPrice = new BigDecimal(0);
+        this.tax = new BigDecimal(0);
+        this.grossPrice = new BigDecimal(0);
+        this.products = new ArrayList<>();
     }
 
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
         products.add(product);
+        BigDecimal newNetPrice = getNetPrice().add(product.getPrice());
+        setNetPrice(newNetPrice);
+
+        BigDecimal newTax = getTax().add(product.getTaxPercent().multiply(product.getPrice()));
+        setTax(newTax);
+
+        BigDecimal newGrossPrice = getGrossPrice().add(product.getPriceWithTax());
+        setGrossPrice(newGrossPrice);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
-        products.put(product,quantity);
+        if (quantity == null || quantity <= 0) {
+            throw new IllegalArgumentException("Product quantity cannot be null or quantity must be greater than 0");
+        }
+        for (int i = 0; i < quantity; i++) {
+            addProduct((product));
+        }
     }
 
     public BigDecimal getNetPrice() {
-        BigDecimal result = BigDecimal.ZERO;
-        for (Product product : this.products){
-            result.add(product.getPrice());
-        }
-        return result;
+        return netPrice;
     }
 
     public BigDecimal getTax() {
-        return BigDecimal.ZERO;
+//        return BigDecimal.ZERO;
+        return tax;
     }
 
     public BigDecimal getGrossPrice() {
-        return BigDecimal.ZERO;/*subTotal.add(subTotal.multiply(tax));*/
+        return grossPrice;
+    }
+
+    public void setNetPrice(BigDecimal netPrice) {
+        this.netPrice = netPrice;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public void setGrossPrice(BigDecimal grossPrice) {
+        this.grossPrice = grossPrice;
     }
 }
